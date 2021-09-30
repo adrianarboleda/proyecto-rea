58a02e3bb2494c75804edd546b5678d8a5a01121
Adrián Arboleda <adrian.arboleda@alumnos.udg.mx>
2021-09-30 09:40:40 -0600

    feature - App, Home, Login, Register, package.json - se inició con el LOGIN

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import LoginScreen from './components/Login';
+import AboutScreen from './components/Register';
+import HomeScreen from './components/Home';
+
+
+class HomeScreen extends React.Component() {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  Home:{
+    screen: LoginScreen
   },
+  About:{
+    screen: AboutScreen
+  }
 });
+
+export default createAppContainer(AppNavigator);
+
